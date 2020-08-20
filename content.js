3218bccffd8e8db0184b2879879f4da9d4100315
Amy Yee <amyyee1998@gmail.com>
2020-08-20 11:21:10 -0400

    second commit

--- content.js
+++ content.js
@@ -1,21 +1,38 @@
 // Once we hit button, function inside addListener performs
-chrome.runtime.onMessage.addListener(function(request) {
-    let obj = {}
-    obj['input'] = request;
-    console.log(obj)
-    
+chrome.runtime.onMessage.addListener(function (request) {
+
     if (request) {
-        let body = document.querySelector("*")
-            body.remove();
-            // reminder pops up
+        //  alert first
+        alert(`No problemo! We'll remind you to: ${request}`)
 
-            // audio and backgound black out next, reminder disappears
-        setTimeout(function (){
+        setTimeout(function () {
+            let pageContent = document.querySelector("body")
 
-            let audio = new Audio('nmh_scream1.mp3');
-            document.appendChild(audio);
-            let played = document.querySelector('audio').play();
-        }, 10000)
-        
+            let divs = document.querySelectorAll('div')
+
+            divs.forEach((el) => {
+                el.parentNode.removeChild(el)
+            })
+
+            let newText = document.createElement('header');
+            let text = document.createTextNode(`GO DO: ${request.toUpperCase()}!!`)
+            newText.appendChild(text)
+            pageContent.appendChild(newText);
+            newText.style.fontSize = "200px"
+            newText.style.textAlign = "center"
+
+            let image = document.createElement("img")
+            image.src = "https://media.giphy.com/media/WKqwoSJsHlxba/giphy.gif"
+            pageContent.appendChild(image);
+            image.style.textAlign = "center"
+
+            let audio = document.createElement("AUDIO")
+            audio.src = "nmh_scream1.mp3"
+            pageContent.appendChild(audio)
+            audio.autoPlay = true;
+
+
+        }, 2000)
+
     }
 })
